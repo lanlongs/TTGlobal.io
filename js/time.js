572370aa6ca06c012ac50427cb731207ea91003f
lanlongs <1946388916@qq.com>
2021-12-31 10:05:29 +0800

    demo

--- js/time.js
+++ js/time.js
@@ -1,5 +1,5 @@
 window.onload=function starttime(){
-        time(h1,'2025/5/1 18:00:00');     // 添加时间
+        time(h1,'2024/3/15 18:00:00');     // 添加时间
         ptimer = setTimeout(starttime,1000); // 添加计时器
 }
     function time(obj,futimg){
@@ -11,5 +11,5 @@ window.onload=function starttime(){
         var hour = parseInt(time/3600)-24*day;    // 小时 60*60 总小时数-过去的小时数=现在的小时数
         var minute = parseInt(time%3600/60); // 分 -(day*24) 以60秒为一整份 取余 剩下秒数 秒数/60 就是分钟数
         var second = parseInt(time%60);  // 以60秒为一整份 取余 剩下秒数
-        obj.innerHTML="距离进藏还有：<br>"+day+"天"+hour+"小时"+minute+"分"+second+"秒"+"<br><span></span>"
+        obj.innerHTML="时间正在一分一秒的流逝：<br>"+day+"天"+hour+"小时"+minute+"分"+second+"秒"+"<br><span>每个人都会被别人改变，很少有人能真正的影响别人</span>"
 }
